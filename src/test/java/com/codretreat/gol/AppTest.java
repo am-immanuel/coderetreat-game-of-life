aa4cf639050fd13478d52968191cf3c5626a1c5d
Immanuel Scheerer <immanuel.scheerer@apparentmedia.de>
2015-11-14 15:57:49 +0100

    Added test for stay alive.

--- src/test/java/com/codretreat/gol/AppTest.java
+++ src/test/java/com/codretreat/gol/AppTest.java
@@ -40,9 +40,21 @@ public class AppTest {
 	
 	@Test
 	public void testOverCrowding(){
-		Grid grid = new Grid(new Point[] { new Point(0, 0), new Point(0, 1), new Point(1, 0), new Point(1, 2) });
+		Grid grid = new Grid(new Point[] { new Point(0, 0), new Point(0, 1), new Point(1, 0), new Point(1, 2), new Point(1, 1) });
 		Cell cell = grid.getCell(1, 1);
+		assertTrue(cell.isAlive());
 		grid.recalculate();
 		assertFalse(cell.isAlive());
+	}
+
+	@Test
+	public void testStayAlive(){
+		Grid grid = new Grid(new Point[] { new Point(0, 0), new Point(0, 1), new Point(1, 0), new Point(1, 2), new Point(1, 1) });
+		grid.recalculate();
+		assertTrue(grid.getCell(1, 2).isAlive());
+		assertTrue(grid.getCell(0, 0).isAlive());
+		assertTrue(grid.getCell(1, 0).isAlive());
+		assertFalse(grid.getCell(0, 1).isAlive());
+		assertFalse(grid.getCell(1, 1).isAlive());
 	}
 }
