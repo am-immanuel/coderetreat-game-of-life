16edd634df1c9da4441bf87211966c3589cd2b26
Immanuel Scheerer <immanuel.scheerer@apparentmedia.de>
2015-11-14 12:09:08 +0100

    added Point and hashmap

--- src/test/java/com/codretreat/gol/Grid.java
+++ src/test/java/com/codretreat/gol/Grid.java
@@ -1,10 +1,24 @@
 package com.codretreat.gol;
 
+import java.util.HashMap;
+import java.util.Map;
+
 public class Grid {
 
+	private Map<Point, Cell> cells = new HashMap<>();
+	
 	public Cell getCell(int i, int j) {
-		// TODO Auto-generated method stub
 		return new Cell();
 	}
 
+	private class Point {
+		public final int x;
+		public final int y;
+		public Point(int x, int y) {
+			super();
+			this.x = x;
+			this.y = y;
+		}
+				
+	}
 }
